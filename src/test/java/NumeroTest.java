2d8666637c646ddb40aacf0c31582f246c8a6c7b
arthurlamberti <arthurlamberti001@gmail.com>
2020-06-07 11:51:01 -0300

    teste numero

---
+++ src/test/java/NumeroTest.java
@@ -0,0 +1,71 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NumeroTest {
+    Numero numero;
+
+    @Test
+    public void deveIncrementarComValorPositivo(){
+        numero = new Numero(4);
+
+        int resultadoEsperado = 5;
+        int resultadoObtido = numero.inc().valor();
+
+        assertEquals(resultadoEsperado,resultadoObtido);
+    }
+
+    @Test
+    public void deveIncrementarComValorNegativo(){
+        numero = new Numero(-4);
+
+        int resultadoEsperado = -3;
+        int resultadoObtido = numero.inc().valor();
+
+        assertEquals(resultadoEsperado,resultadoObtido);
+    }
+
+    @Test
+    public void deveDecrementarComValorPositivo(){
+        numero = new Numero(4);
+
+        int resultadoEsperado = 3;
+        int resultadoObtido = numero.dec().valor();
+
+        assertEquals(resultadoEsperado,resultadoObtido);
+    }
+
+    @Test
+    public void deveDecrementarComValorNegativo(){
+        numero = new Numero(-4);
+
+        int resultadoEsperado = -5;
+        int resultadoObtido = numero.dec().valor();
+
+        assertEquals(resultadoEsperado,resultadoObtido);
+    }
+
+    @Test
+    public void deveRetornarValorAbsoluto(){
+        numero = new Numero(-4);
+
+        int resultadoEsperado = 4;
+        int resultadoObtido = numero.abs().valor();
+
+        assertEquals(resultadoEsperado,resultadoObtido);
+    }
+
+    @Test
+    public void deveDuplicarNumero(){
+        numero = new Numero(7);
+        Numero numeroDuplicado = numero.duplica();
+
+
+        int resultadoEsperado = 7;
+        int resultadoObtido = numeroDuplicado.valor();
+
+        assertEquals(resultadoEsperado,resultadoObtido);
+        assertNotSame(numero, numeroDuplicado);
+    }
+
+}

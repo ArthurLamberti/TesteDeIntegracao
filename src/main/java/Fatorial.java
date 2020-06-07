6b5b0ad5c082b84917f2c8a08cb5112daffc24a2
AndrewsSouza <andrews.souza@outlook.com.br>
2020-06-07 02:41:51 -0300

    feat: multiplicador e fatorial

---
+++ src/main/java/Fatorial.java
@@ -0,0 +1,29 @@
+package com.bcopstein;
+
+import com.bcopstein.Numero;
+
+public class Fatorial {
+    private Numero resultado;
+    private Multiplicador multiplicador;
+
+    Fatorial(Multiplicador multiplicador) {
+        this.resultado = new Numero(0);
+        this.multiplicador = multiplicador;
+    }
+
+    public Fatorial fatorial(Numero valor) {
+        multiplicador.set(valor.duplica());
+
+        while (valor.valor() > 1) {
+            multiplicador.vezes(valor);
+            valor.dec();
+        }
+
+        this.resultado = multiplicador.resultado();
+        return this;
+    }
+
+    public Numero resultado() {
+        return resultado;
+    }
+}
---
+++ src/main/java/Multiplicador.java
@@ -0,0 +1,58 @@
+package com.bcopstein;
+
+import com.bcopstein.Somador;
+
+public class Multiplicador {
+    private Numero resultado;
+    private Somador somador;
+
+    public Multiplicador(Somador somador) {
+        this.resultado = new Numero(0);
+        this.somador = somador;
+    }
+
+    public Multiplicador set(Numero numero) {
+        this.resultado = numero;
+        return this;
+    }
+
+    public Multiplicador vezes(Numero outro) {
+        outro = outro.duplica();
+
+        somador.set(this.resultado.duplica());
+
+        while (outro.valor() > 0) {
+            somador.mais(this.resultado);
+            outro.dec();
+        }
+
+        this.resultado = somador.resultado();
+        return this;
+    }
+
+    public Multiplicador dividido(Numero outro) {
+        if (outro.valor() == 0) {
+            throw new RuntimeException("Um número não pode ser dividido por 0");
+        }
+
+        if (resultado.valor() % outro.valor() != 0) {
+            throw new RuntimeException("Divisão não é inteira");
+        }
+
+        outro = outro.duplica();
+
+        somador.set(this.resultado.duplica());
+
+        while (outro.valor() > 0) {
+            somador.menos(this.resultado);
+            outro.dec();
+        }
+
+        this.resultado = somador.resultado();
+        return this;
+    }
+
+    public Numero resultado() {
+        return resultado;
+    }
+}
